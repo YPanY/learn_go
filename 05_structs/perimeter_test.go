f838e4c19843ba5458a3fd5cb99298e951168476
YPanY <82483406+YPanY@users.noreply.github.com>
2022-04-06 16:28:29 +0900

    Refactored the tests as Table Driven Tests method

--- 05_structs/perimeter_test.go
+++ 05_structs/perimeter_test.go
@@ -12,27 +12,49 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+//func TestArea(t *testing.T) {
+//
+//	checkArea := func(t testing.TB, shape Shape, want float64) {
+//		t.Helper()
+//		got := shape.Area()
+//		if got != want {
+//			t.Errorf("got %g want %g", got, want)
+//		}
+//	}
+//
+//	t.Run("rectangles", func(t *testing.T) {
+//		rectangle := Rectangle{12.0, 6.0}
+//		want := 72.0
+//
+//		checkArea(t, rectangle, want)
+//	})
+//
+//	t.Run("circles", func(t *testing.T) {
+//		circle := Circle{10}
+//		want := 314.1592653589793
+//
+//		checkArea(t, circle, want)
+//	})
+//}
+
+// Refactoring the TestArea with Table Driven Tests method
 func TestArea(t *testing.T) {
-
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		want := 72.0
-
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-
-		checkArea(t, circle, want)
-	})
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
