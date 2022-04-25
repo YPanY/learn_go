2e5bed4d333e6871a8ea8d87b1af0c4fdccb4cb4
YPanY <pan.coding.2021@gmail.com>
2022-04-25 22:52:32 +0900

    maps

---
+++ 07_maps/dictionary_test.go
@@ -0,0 +1,33 @@
+package maps
+
+import "testing"
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertString(t, err.Error(), want)
+	})
+}
